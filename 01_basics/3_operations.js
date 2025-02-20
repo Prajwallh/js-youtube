995478594458d584699a8892364ace518104d62d
Prajwal L h <163243344+Prajwallh@users.noreply.github.com>
2025-02-20 09:21:47 +0000

    comparision operators and filename update

---
+++ 01_basics/3_operations.js
@@ -0,0 +1,34 @@
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  // 2 power 3
+// console.log(2/3);
+// console.log(2%2);
+
+//Concatination of strings
+// let str1 = "Hi";
+// let str2 = " Javascript";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("2" + 1);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// //use paranthisis to be specific
+// console.log((3 + 4) * 5 % 3);
+
+//prefix and postfix operators
+gameCounter = 100;
+console.log(++gameCounter);
+console.log(gameCounter++);
+
+
+
+
---
+++ 01_basics/4_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); THESE ARE SOME OF BASIC COMPARISON OPERATORS
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//Only in js diffrent dataypes can be compared...JS converts "02 or 2" from string to number
+
+console.log(null > 0);
+console.log((null == 0));
+console.log(null >= 0);
+
+//The raeson is that an equality check == and comparisons > < >= <= work differntly comparisons converty null into 0 thats why its true(3)
+
+// < and == and === and >= these operators work differntly in Js
+
+
+// Strict check ( === )
+console.log("2" === 2);
+
+
+
+
